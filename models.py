8e059a4d3bb4b7709f4970e7abd9627146dcab2e
YimingZhou <yimingzhou0416@gmail.com>
2020-03-16 18:32:08 -0700

    add model13

--- models.py
+++ models.py
@@ -65,20 +65,22 @@ class CNN2LSTM(nn.Module):
     CNN1 + LSTM1 + CNN2 + LSTM2
     
     '''
-    def __init__(self, cnn1_input_size, rnn1_input_size, hidden_size1, cnn2_input_size, rnn2_input_size, hidden_size2, output_dim, dropout):
-        super(CNNLSTMnet, self).__init__()
-        self.cnn_input_size = cnn_input_size
-        self.rnn_input_size = rnn_input_size
-        self.hidden_size = hidden_size
+    def __init__(self, cnn1_input_size, rnn1_input_size, hidden_size1, rnn2_input_size, hidden_size2, output_dim, dropout):
+        super(CNN2LSTM, self).__init__()
+        self.cnn1_input_size = cnn1_input_size
+        self.rnn1_input_size = rnn1_input_size
+        self.rnn2_input_size = rnn2_input_size
+        self.hidden_size1 = hidden_size1
+        self.hidden_size2 = hidden_size2
         self.output_dim = output_dim
         self.cnn1 = nn.Sequential(
             nn.Conv1d(cnn1_input_size, rnn1_input_size, kernel_size=10, stride=2),
-            nn.BatchNorm1d(rnn_input_size),
+            nn.BatchNorm1d(rnn1_input_size),
             nn.ReLU(),
         )
         self.rnn1 = nn.LSTM(input_size=rnn1_input_size, hidden_size=hidden_size1, num_layers=1, dropout=dropout)
         self.cnn2 = nn.Sequential(
-            nn.Conv1d(cnn2_input_size, rnn2_input_size, kernel_size=10, stride=2),
+            nn.Conv1d(hidden_size1, rnn2_input_size, kernel_size=10, stride=2),
             nn.BatchNorm1d(rnn2_input_size),
             nn.ReLU(),
         )
@@ -87,18 +89,12 @@ class CNN2LSTM(nn.Module):
     
     def forward(self, x, h=None):
         out = self.cnn1(x)
-        # out (batch, feature_num, seq_len)
         out = out.permute(2,0,1)
-        # out (seq_len, batch, feature_num)
-        if type(h) == type(None):
-            out, hn = self.rnn(out)
-        else:
-            out, hn = self.rnn(out, h.detach())        
-        # out (seq_len, batch, feature_num)
-        out = out.permute(1, 2, 0)
-        # out (batch, feature_num, seq_len)
-
-
+        out, hn = self.rnn1(out)      
+        out = out.permute(1, 2, 0)        
+        out = self.cnn2(out)
+        out = out.permute(2,0,1)
+        out, hn = self.rnn2(out) 
         out = self.fc(out[-1, :, :])
         return out
 
@@ -151,5 +147,68 @@ class CNNGRUnet(nn.Module):
             out, hn = self.rnn(out)
         else:
             out, hn = self.rnn(out, h.detach())
+        out = self.fc(out[-1, :, :])
+        return out
+
+
+class CNN2GRUnet(nn.Module):
+    '''
+    Conv(temporal) + Conv(spatial) + GRU
+    
+    '''
+    def __init__(self, hidden_size, output_dim, dropout):
+        super(CNN2GRUnet, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_dim = output_dim
+        # conv on temporal
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(1, 10), stride=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+        )
+        # conv on spatial 
+        self.cnn2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(22, 1), stride=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d((1,4)),
+        )
+        
+        # conv on temporal
+        self.cnn3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 10), stride=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d((1,4)),
+        )
+        # conv on temporal
+    
+        self.cnn4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 10), stride=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d((1,4)),
+            nn.Dropout(p=dropout)
+        )
+    
+        self.rnn1 = nn.GRU(input_size=128, hidden_size=64, num_layers=1)
+        self.rnn2 = nn.GRU(input_size=64, hidden_size=32, num_layers=1)
+        self.fc = nn.Linear(32, output_dim)
+    
+    def forward(self, x, h=None):
+        x = torch.unsqueeze(x, 1)
+        out = self.cnn1(x)
+        out = self.cnn2(out)        
+        out = self.cnn3(out)
+        out = self.cnn4(out)    
+        out = torch.squeeze(out)
+        out = out.permute(2,0,1)
+
+        if type(h) == type(None):
+            out, hn = self.rnn1(out)
+        else:
+            out, hn = self.rnn1(out, h.detach())
+        out, hn = self.rnn2(out)
         out = self.fc(out[-1, :, :])
         return out
